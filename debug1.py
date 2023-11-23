baf870864ada5d4c94494cebf3d04f77a92c859c
Anup Jha <anup.jha6@gmail.com>
2023-11-23 12:13:15 +0530

    debugging completed

---
+++ debug1.py
@@ -0,0 +1,19 @@
+# Sample Python code
+def add(a, b):
+    result = a + b
+    return result
+
+def subtract(a, b):
+    result = a - b
+    return result
+
+def main():
+    x = 10
+    y = 5
+    z = add(x, y)
+    w = subtract(x, y)
+    print("Sum:", z)
+    print("Difference:", w)
+
+if __name__ == "__main__":
+    main()
